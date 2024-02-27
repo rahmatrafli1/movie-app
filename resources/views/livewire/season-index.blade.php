d507417f9bf56942f1cb7b94c2ae9cd54a47a180
rahmatrafli1 <rahmatrafli1@gmail.com>
2024-02-27 10:47:20 +0700

    add tailwind css responsive table

--- resources/views/livewire/season-index.blade.php
+++ resources/views/livewire/season-index.blade.php
@@ -1,3 +1,33 @@
-<div>
-    <h1 class="text-2xl font-bold">Season Index</h1>
-</div>
+<section class="container mx-auto p-6 font-mono">
+    <div class="w-full flex mb-4 p-2 justify-end">
+        <x-button>Create Season</x-button>
+    </div>
+    <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
+        <div class="w-full overflow-x-auto">
+            <table class="w-full">
+                <thead>
+                    <tr
+                        class="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
+                        <th class="px-4 py-3">Title</th>
+                        <th class="px-4 py-3">Release Date</th>
+                        <th class="px-4 py-3">Rating</th>
+                        <th class="px-4 py-3">Public</th>
+                        <th class="px-4 py-3">Manage</th>
+                    </tr>
+                </thead>
+                <tbody class="bg-white">
+                    <tr class="text-gray-700">
+                        <td class="px-4 py-3 border">
+                            Title Here
+                        </td>
+                        <td class="px-4 py-3 border">Date</td>
+                        <td class="px-4 py-3 border">
+                            5
+                        </td>
+                        <td class="px-4 py-3 border">Yes</td>
+                        <td class="px-4 py-3 border">Edit</td>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</section>
