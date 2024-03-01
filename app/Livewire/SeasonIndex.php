c8427988c1859d98c544eda34c7b9a34decdf31c
rahmatrafli1 <rahmatrafli1@gmail.com>
2024-03-01 14:11:10 +0700

    work with seasons

--- app/Livewire/SeasonIndex.php
+++ app/Livewire/SeasonIndex.php
@@ -2,12 +2,19 @@
 
 namespace App\Livewire;
 
+use App\Models\Season;
 use Livewire\Component;
 
 class SeasonIndex extends Component
 {
+    public $search = '';
+    public $sort = 'asc';
+    public $perPage = 5;
+
     public function render()
     {
-        return view('livewire.season-index');
+        return view('livewire.season-index', [
+            'seasons' => Season::search('name', $this->search)->orderBy('name', $this->sort)->paginate($this->perPage)
+        ]);
     }
 }
--- resources/views/livewire/serie-index.blade.php
+++ resources/views/livewire/serie-index.blade.php
@@ -76,6 +76,8 @@
                             </td>
                             <td class="px-4 py-3 border font-semibold">{{ $serie->slug }}</td>
                             <td class="px-4 py-3 border">
+                                <a href="{{ route('admin.seasons.index', $serie->id) }}"
+                                    class="inline-flex items-center px-4 py-2 bg-slate-700 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-slate-800 focus:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 transition ease-in-out duration-150">Season</a>
                                 <x-m-button wire:click="showDetailModal({{ $serie->id }})"
                                     class="bg-blue-500 hover:bg-blue-800">Show</x-m-button>
                                 <x-m-button wire:click="showEditModal({{ $serie->id }})"
