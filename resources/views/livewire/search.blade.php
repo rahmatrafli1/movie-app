68a987850ca1fe99e93831656a29a816f054f91a
rahmatrafli1 <rahmatrafli1@gmail.com>
2024-03-18 09:20:01 +0700

    Adding Loading State

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -17,8 +17,24 @@
                 <input wire:model.live="search" type="text" class="rounded w-full dark:bg-gray-700" id="searchMovie"
                     autocomplete="off">
 
+                <div wire:loading class="border border-blue-300 shadow rounded-md p-4 mt-2 w-full mx-auto">
+                    <div class="animate-pulse flex space-x-4">
+                        <div class="rounded-full bg-slate-700 h-10 w-10"></div>
+                        <div class="flex-1 space-y-6 py-1">
+                            <div class="h-2 bg-slate-700 rounded"></div>
+                            <div class="space-y-3">
+                                <div class="grid grid-cols-3 gap-4">
+                                    <div class="h-2 bg-slate-700 rounded col-span-2"></div>
+                                    <div class="h-2 bg-slate-700 rounded col-span-1"></div>
+                                </div>
+                                <div class="h-2 bg-slate-700 rounded"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 @if (!empty($search))
-                    <div class="w-full">
+                    <div class="w-full" wire:loading.remove>
                         @if (!empty($searchResults))
                             @foreach ($searchResults as $searchResult)
                                 <h1 class="text-white font-bold">{{ $searchResult['type'] }}</h1>
